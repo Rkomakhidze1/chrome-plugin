334049ce47d13ce73166c2f00b7c422e0dbfd184
Rkomakhidze1 <rezokomakhidze1@gmail.com>
2021-10-09 01:14:27 +0400

    connected to the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [storyCount, setStoryCount] = useState(0);
+  const url = 'http://localhost:3001/test';
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((storyList) => {
+        console.log(storyList);
+        setStoryCount(storyList.length);
+      })
+      .catch((e) => {
+        console.log('something went wrong');
+        console.log(e);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Hello chrome</h1>
+      <p>story count: {storyCount}</p>
+    </>
+  );
 }
 
 export default App;
